f8ba0a995241b53ab00d22a1cd7ee0e30181a7f1
ITMAN <ITMAN@DESKTOP-IKVCGFO>
2017-02-08 10:53:16 +0700

    update start method

--- config/params.php
+++ config/params.php
@@ -8,12 +8,12 @@ return [
     'provcode' => '72',
     'ampcode' => '', #เลข 4 หลัก
     'codebase' => '00056',
-    'cusername' => 'admin',
-    'cpassword' => 'qw2267er',
+    'cusername' => '',
+    'cpassword' => '',
     'webserviceUrl' => 'http://127.0.0.1/yiiproject/web/index.php/webservice', #'http://61.19.87.182/wmc/web/index.php/webservice',
-    'wsDataHdcUrl' => 'http://113.53.232.5/wmc/web/index.php/hdcsync', #'http://61.19.87.182/wmc/web/index.php/webservice',
+    'wsDataHdcUrl' => 'http://127.0.0.1/wmc/web/index.php/hdcsync', #'http://61.19.87.182/wmc/web/index.php/webservice',
     'wsUsername' => '7206',
-    'wsPassword' => '$2y$10$KJ3vOAWS9Ckwk3IczFFP2uXnYR9lAHqOjTdfNMNLA0X8nBP8nqfua',
+    'wsPassword' => '',
     'bootswatchTheme' => 'Flatly', #Journal cerulean||yeti Cyborg ,Cosmo ,Flatly,Journal,Lumen,Superhero,United,Simplex,Slate,Darkly,cerulean
     'icon-framework' => 'fa', # Font Awesome Icon framework
     'adminLteTheme' => 'skin-blue', #skin-blue,skin-black,skin-red,skin-yellow,skin-purple,skin-green,skin-blue-light,skin-black-light,skin-red-light,skin-yellow-light,skin-purple-light,skin-green-light
