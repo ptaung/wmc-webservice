ab98aaa9203c080ab212109eee49d329be20c534
Sila Klanklaeo <p_taung@hotmail.com>
2017-11-24 08:22:31 +0700

    Update db.php

--- config/db.php
+++ config/db.php
@@ -1,27 +1,11 @@
 <?php
 
-/*
-  return [
-  'class' => 'yii\db\Connection',
-  'dsn' => 'mysql:host=192.168.9.99;port=3306;dbname=pcu_audit',
-  'username' => 'ptaung',
-  'password' => 'qw2267er',
-  'charset' => 'utf8',
-  'enableSchemaCache' => true,
-  // Duration of schema cache.
-  'schemaCacheDuration' => 3600,
-  // Name of the cache component used to store schema information
-  'schemaCache' => 'cache',
-  'tablePrefix' => 'wm_',
-  ];
- */
-
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=192.168.9.9;port=3306;dbname=pcu_audit',
-    'username' => 'ptaung',
-    'password' => 'qw2267er',
+    'dsn' => 'mysql:host=127.0.0.1;port=3306;dbname=wm_webservice',
+    'username' => '',
+    'password' => '',
     'charset' => 'utf8',
     'enableSchemaCache' => true,
     // Duration of schema cache.
