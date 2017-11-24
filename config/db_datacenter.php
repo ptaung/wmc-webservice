6f7023c03cc09227598c52d030a97a9a02efa851
Sila Klanklaeo <p_taung@hotmail.com>
2017-11-24 08:23:10 +0700

    Update db_datacenter.php

--- config/db_datacenter.php
+++ config/db_datacenter.php
@@ -1,26 +1,10 @@
 <?php
 
-/*
-  return [
-  'class' => 'yii\db\Connection',
-  'dsn' => 'mysql:host=127.0.0.1;dbname=wm2016',
-  'username' => 'sa',
-  'password' => 'sa',
-  'charset' => 'utf8',
-  'enableSchemaCache' => true,
-  // Duration of schema cache.
-  'schemaCacheDuration' => 3600,
-  // Name of the cache component used to store schema information
-  'schemaCache' => 'cache',
-  ];
- */
-
-
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=192.168.9.99;port=3306;dbname=pcu_audit',
-    'username' => 'ptaung',
-    'password' => 'qw2267er',
+    'dsn' => 'mysql:host=127.0.0.1;port=3306;dbname=wm_webservice',
+    'username' => '',
+    'password' => '',
     'charset' => 'utf8',
     'enableSchemaCache' => true,
     // Duration of schema cache.
@@ -28,4 +12,6 @@ return [
     // Name of the cache component used to store schema information
     'schemaCache' => 'cache',
 ];
+
+
 
